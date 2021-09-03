36b1680365890133bcc378e983885d7c6f5f2834
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-09-04 00:01:18 +0530

    leap year

--- functional_problems/Program.cs
+++ functional_problems/Program.cs
@@ -1,12 +1,20 @@
 ﻿using System;
-
-namespace functional_problems
+namespace CheckLeapYear
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("FUNCTIONAL PROGRAMMMING PROBLEMS");
+            Console.WriteLine("Enter Year : ");
+            int Year = int.Parse(Console.ReadLine());
+            if (Year % 4 == 0)
+            { Console.WriteLine("{0} is a Leap Year.", Year); }
+
+            else
+            {
+                Console.WriteLine("{0} is not a Leap Year.", Year);
+            }
+                Console.ReadLine();
         }
     }
 }
