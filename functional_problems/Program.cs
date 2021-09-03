5809e16893b4d2042886c6864b17e90c0af9daac
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-09-03 23:55:07 +0530

    coin flip percentang

--- functional_problems/Program.cs
+++ functional_problems/Program.cs
@@ -1,12 +1,34 @@
 ﻿using System;
 
-namespace functional_problems
+namespace CoinFlip
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("FUNCTIONAL PROGRAMMMING PROBLEMS");
+            int a = 0;
+            int b = 0;
+            double per1;
+            double per2;
+            Random random = new Random();
+            for (int i = 0; i < 100; i++)
+            { 
+                int toss = random.Next(0, 2);
+                if (toss == 0)
+                {
+                     a += 1;
+                }
+                else if(toss == 1)
+                {
+                    b += 1;
+                }
+             }
+            Console.WriteLine("heads:" + a);
+            Console.WriteLine("tails:" + b);
+            per1 = a * 100;
+            per2 = b * 100;
+            Console.WriteLine("heads percentage:" + per1/100);
+            Console.WriteLine("tails percentage:" + per2/100);
         }
     }
 }
