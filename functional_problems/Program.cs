b6a9f5d0541035c45e3d9ecaf8910b0ed3dfe615
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-09-03 23:01:37 +0530

    first commit

---
+++ functional_problems/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace functional_problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("FUNCTIONAL PROGRAMMMING PROBLEMS");
+        }
+    }
+}
