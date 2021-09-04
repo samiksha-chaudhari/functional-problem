dd39471753a787b0aeef16b66761b34f17b43920
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-09-04 15:24:48 +0530

    largest number

--- functional_problems/Program.cs
+++ functional_problems/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 
-namespace functional_problems
+namespace myApp
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("FUNCTIONAL PROGRAMMMING PROBLEMS");
+            Console.WriteLine("Enter number1:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number2:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number3:");
+            int c = Convert.ToInt32(Console.ReadLine());
+            
+            if ((a > b) && (a > c))
+                Console.WriteLine("a = {0} is the biggest number", a);
+
+            else if (b > c)
+                Console.WriteLine("b = {0} is the biggest number", b);
+
+            else
+                Console.WriteLine("c = {0} is the biggest number", c);
         }
     }
 }
