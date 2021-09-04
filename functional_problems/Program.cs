46fb98e2150f866d89a1369c35c90ddaf5247ef2
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-09-04 10:26:06 +0530

    harmonic number addition

--- functional_problems/Program.cs
+++ functional_problems/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
-
-namespace functional_problems
+class HARMONIC
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("FUNCTIONAL PROGRAMMMING PROBLEMS");
-        }
-    }
+
+	static float sum(int n)
+	{
+		double i, s = 0.0;
+
+		for (i = 1; i <= n; i++)
+			s = s + 1 / i;
+
+		return (float)s;
+	}
+	public static void Main()
+	{
+		Console.WriteLine("enter number:");
+		int n = Convert.ToInt32(Console.ReadLine());
+
+		Console.WriteLine("Sum is "
+						+ sum(n));
+	}
 }
