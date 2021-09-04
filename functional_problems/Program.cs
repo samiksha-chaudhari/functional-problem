28956e9b6ae7a6befefe01b60751d5a08cbdb5f1
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-09-04 09:34:25 +0530

    change

--- functional_problems/Program.cs
+++ functional_problems/Program.cs
@@ -4,8 +4,9 @@ class POWER
 {	public static void Main()
 	{
 		int value = 2;
-		for (int power = 0; power <= 32; power++)
-			Console.WriteLine($"{value}^{power} = {(long)Math.Pow(value, power):N0} (0x{(long)Math.Pow(value, power):X})");
 
+		
+
+		
 	}
 }
