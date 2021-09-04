530756eccb6556b3952438312213ca50b67ea941
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-09-04 14:57:24 +0530

    swapping numbers

--- functional_problems/Program.cs
+++ functional_problems/Program.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 
-namespace functional_problems
+namespace myApp
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("FUNCTIONAL PROGRAMMMING PROBLEMS");
-        }
-    }
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Enter number1:");
+			int num1 = Convert.ToInt32(Console.ReadLine());
+			Console.WriteLine("Enter number2:");
+			int num2 = Convert.ToInt32(Console.ReadLine());
+			
+			int temp = num1;
+			num1 = num2;
+			num2 = temp;
+
+			Console.WriteLine("After swapping...");
+			Console.WriteLine("num1 = {0}", num1);
+			Console.WriteLine("num2 = {0}", num2);
+		}
+	}
 }
