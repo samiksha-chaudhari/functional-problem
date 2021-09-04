6b0083d53f47d51b963de5b5d9416c65bcdd2261
samiksha-chaudhari <spchaudhari80@gmail.com>
2021-09-04 15:20:05 +0530

    check vowel or consonant

--- functional_problems/Program.cs
+++ functional_problems/Program.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 
-namespace functional_problems
+public class CHAR
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        char ch;
+
+        Console.WriteLine("Enter any character: ");
+        ch = Convert.ToChar(Console.ReadLine());
+
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || 
+            ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
         {
-            Console.WriteLine("FUNCTIONAL PROGRAMMMING PROBLEMS");
+
+            Console.WriteLine(ch + " is Vowel.");
+
         }
+        else 
+        {
+            Console.WriteLine(ch + " is Consonant.");
+        }
+
+        Console.ReadLine();
     }
 }
